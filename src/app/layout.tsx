22226072471013f6787967bfbea415d59e8c4726
Helloflixofficial <12345678+Helloflixofficial@users.noreply.github.com>
2026-02-06 22:16:58 +0530

    02:Auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,17 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { dark } from "@clerk/themes"
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import { Button } from '@/components/ui/button';
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
@@ -24,15 +34,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.variable} ${plexMono.variable} antialiased`}
-      >
-        <ThemeProvider attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange>{children}</ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider appearance={{ theme: dark }}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.variable} ${plexMono.variable} antialiased`}
+        >
+          <ThemeProvider attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange>
+            <header>
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton>
+                  <Button className="bg-blue-500 text-white rounded-md px-4 py-2">Sign-UP</Button>
+                </SignUpButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
